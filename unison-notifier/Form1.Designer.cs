b9daac955576c96e51f73fae9e9a700aa6835e3e
Nicholas Pitt <ngpitt@iecfusor.com>
2015-05-12 02:45:46 -0400

    Added codebase

---
+++ unison-notifier/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace unison_notifier
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+      this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.SuspendLayout();
+      // 
+      // notifyIcon1
+      // 
+      this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+      this.notifyIcon1.BalloonTipText = "Unison is connecting...";
+      this.notifyIcon1.BalloonTipTitle = "Connecting";
+      this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+      this.notifyIcon1.Text = "Unison";
+      this.notifyIcon1.Visible = true;
+      // 
+      // richTextBox1
+      // 
+      this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+      this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+      this.richTextBox1.Name = "richTextBox1";
+      this.richTextBox1.ReadOnly = true;
+      this.richTextBox1.Size = new System.Drawing.Size(560, 237);
+      this.richTextBox1.TabIndex = 0;
+      this.richTextBox1.Text = "";
+      this.richTextBox1.WordWrap = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(584, 261);
+      this.Controls.Add(this.richTextBox1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "Form1";
+      this.ShowInTaskbar = false;
+      this.Text = "Unison";
+      this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.NotifyIcon notifyIcon1;
+    private System.Windows.Forms.RichTextBox richTextBox1;
+  }
+}
+
---
+++ unison-notifier/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Drawing;
+
+namespace unison_notifier
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+
+      unisonProcess.StartInfo.FileName = "unison.exe";
+      unisonProcess.StartInfo.RedirectStandardError = true;
+      unisonProcess.StartInfo.UseShellExecute = false;
+      unisonProcess.StartInfo.CreateNoWindow = true;
+
+      Load += new EventHandler(Form1_Load);
+      Resize += new EventHandler(Form1_Resize);
+      FormClosing += new FormClosingEventHandler(Form1_Closing);
+
+      ContextMenu contextMenu = new ContextMenu();
+      MenuItem logMenuItem = new MenuItem(),
+        settingsMenuItem = new MenuItem(),
+        exitMenuItem = new MenuItem();
+
+      logMenuItem.Text = "Log";
+      logMenuItem.Click += new EventHandler(logMenuItem_Click);
+      contextMenu.MenuItems.Add(logMenuItem);
+
+      settingsMenuItem.Text = "Settings";
+      settingsMenuItem.Click += new EventHandler(settingsMenuItem_Click);
+      contextMenu.MenuItems.Add(settingsMenuItem);
+
+      contextMenu.MenuItems.Add("-");
+
+      exitMenuItem.Text = "Exit";
+      exitMenuItem.Click += new EventHandler(exitMenuItem_Click);
+      contextMenu.MenuItems.Add(exitMenuItem);
+
+      notifyIcon1.MouseClick += new MouseEventHandler(notifyIcon1_MouseClick);
+      notifyIcon1.ContextMenu = contextMenu;
+
+      notifierThread = new Thread(notifier);
+      notifierThread.Start();
+    }
+
+    private Process unisonProcess = new Process();
+    private Thread notifierThread;
+    private ManualResetEvent notifierResetEvent = new ManualResetEvent(false);
+    private string log;
+    private bool terminate = false, firstTimeShown = true;
+    private delegate void notifyDelegate(Icon icon, string title, string text);
+    private delegate void updateLogDelegate();
+    private delegate void clearLogDelegate();
+
+    private void notifier()
+    {
+      while (!terminate)
+      {
+        unisonProcess.Start();
+
+        while (!unisonProcess.HasExited && !terminate)
+        {
+          string line = unisonProcess.StandardError.ReadLine();
+          
+          if (line != null)
+          {
+            if (line.Contains("Nothing to do"))
+            {
+              safeInvoke(new notifyDelegate(notify), Properties.Resources.SyncedIcon, "Synced", "Unison is connected and synced.");
+            }
+            else if (line.Contains("Propagating updates"))
+            {
+              safeInvoke(new notifyDelegate(notify), Properties.Resources.SyncingIcon, "Syncing", "Unison is syncing files...");
+            }
+
+            log = line + Environment.NewLine;
+            safeInvoke(new updateLogDelegate(updateLog));
+          }
+        }
+
+        safeInvoke(new notifyDelegate(notify), Properties.Resources.ErrorIcon, "Disconnected", "Unison has lost connection.");
+        notifierResetEvent.WaitOne(10000);
+        safeInvoke(new clearLogDelegate(clearLog));
+      }
+    }
+
+    private void safeInvoke(Delegate method, params object[] args)
+    {
+      if (!terminate)
+      {
+        Invoke(method, args);
+      }
+    }
+
+    private void notify(Icon icon, string title, string text)
+    {
+      notifyIcon1.Icon = icon;
+      notifyIcon1.BalloonTipTitle = title;
+      notifyIcon1.BalloonTipText = text;
+    }
+
+    private void updateLog()
+    {
+      richTextBox1.AppendText(log);
+    }
+
+    private void clearLog()
+    {
+      richTextBox1.Clear();
+    }
+
+    private void logMenuItem_Click(object sender, EventArgs e)
+    {
+      if (firstTimeShown)
+      {
+        firstTimeShown = false;
+        WindowState = FormWindowState.Normal;
+        ShowInTaskbar = true;
+        CenterToScreen();
+      }
+      richTextBox1.SelectionStart = richTextBox1.Text.Length;
+      richTextBox1.ScrollToCaret();
+      Show();
+    }
+
+    private void settingsMenuItem_Click(object sender, EventArgs e)
+    {
+      string systemRoot = Environment.ExpandEnvironmentVariables("%systemroot%");
+      string homePath = Environment.ExpandEnvironmentVariables("%homepath%");
+
+      Process.Start(systemRoot + "\\System32\\notepad.exe", homePath + "\\.unison\\default.prf");
+    }
+
+    private void exitMenuItem_Click(object sender, EventArgs e)
+    {
+      terminate = true;
+      Close();
+    }
+
+    private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+    {
+      if (e.Button == MouseButtons.Left)
+      {
+        notifyIcon1.ShowBalloonTip(5000);
+      }
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      Hide();
+    }
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+      richTextBox1.Width = Width - 40;
+      richTextBox1.Height = Height - 63;
+    }
+
+    private void Form1_Closing(object sender, FormClosingEventArgs e)
+    {
+      if (!terminate)
+      {
+        Hide();
+        e.Cancel = true;
+      }
+      else
+      {
+        notifierResetEvent.Set();
+        if (!unisonProcess.HasExited)
+        {
+          unisonProcess.Kill();
+        }
+        notifierThread.Join();
+      }
+    }
+  }
+}
