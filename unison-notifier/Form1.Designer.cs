32060e01a71df8650e809bb287cca528bb04c46b
Nicholas Pitt <ngpitt@iecfusor.com>
2015-05-13 05:59:18 -0400

    Moved design code into the designer

--- unison-notifier/Form1.Designer.cs
+++ unison-notifier/Form1.Designer.cs
@@ -31,14 +31,59 @@
       this.components = new System.ComponentModel.Container();
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
       this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+      this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.statusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+      this.contextMenuStrip1.SuspendLayout();
       this.SuspendLayout();
       // 
       // notifyIcon1
       // 
+      this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
       this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
       this.notifyIcon1.Text = "Unison";
       this.notifyIcon1.Visible = true;
+      this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+      // 
+      // contextMenuStrip1
+      // 
+      this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+      this.contextMenuStrip1.Name = "contextMenuStrip1";
+      this.contextMenuStrip1.Size = new System.Drawing.Size(117, 76);
+      // 
+      // statusToolStripMenuItem
+      // 
+      this.statusToolStripMenuItem.Name = "statusToolStripMenuItem";
+      this.statusToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+      this.statusToolStripMenuItem.Text = "Status";
+      this.statusToolStripMenuItem.Click += new System.EventHandler(this.statusToolStripMenuItem_Click);
+      // 
+      // settingsToolStripMenuItem
+      // 
+      this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+      this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+      this.settingsToolStripMenuItem.Text = "Settings";
+      this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+      // 
+      // toolStripSeparator1
+      // 
+      this.toolStripSeparator1.Name = "toolStripSeparator1";
+      this.toolStripSeparator1.Size = new System.Drawing.Size(113, 6);
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+      this.exitToolStripMenuItem.Text = "Exit";
+      this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
       // 
       // richTextBox1
       // 
@@ -51,6 +96,12 @@
       this.richTextBox1.Text = "";
       this.richTextBox1.WordWrap = false;
       // 
+      // backgroundWorker1
+      // 
+      this.backgroundWorker1.WorkerSupportsCancellation = true;
+      this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+      this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -62,6 +113,10 @@
       this.ShowInTaskbar = false;
       this.Text = "Unison Status";
       this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.Resize += new System.EventHandler(this.Form1_Resize);
+      this.contextMenuStrip1.ResumeLayout(false);
       this.ResumeLayout(false);
 
     }
@@ -70,6 +125,12 @@
 
     private System.Windows.Forms.NotifyIcon notifyIcon1;
     private System.Windows.Forms.RichTextBox richTextBox1;
+    private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem statusToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    private System.ComponentModel.BackgroundWorker backgroundWorker1;
   }
 }
 
--- unison-notifier/Form1.cs
+++ unison-notifier/Form1.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.ComponentModel;
 using System.Threading;
 using System.Drawing;
 
@@ -17,94 +18,36 @@ namespace unison_notifier
       unisonProcess.StartInfo.RedirectStandardError = true;
       unisonProcess.StartInfo.UseShellExecute = false;
       unisonProcess.StartInfo.CreateNoWindow = true;
-
-      Load += new EventHandler(Form1_Load);
-      Resize += new EventHandler(Form1_Resize);
-      FormClosing += new FormClosingEventHandler(Form1_Closing);
-
-      ContextMenu contextMenu = new ContextMenu();
-      MenuItem statusMenuItem = new MenuItem(),
-        settingsMenuItem = new MenuItem(),
-        exitMenuItem = new MenuItem();
-
-      statusMenuItem.Text = "Status";
-      statusMenuItem.Click += new EventHandler(statusMenuItem_Click);
-      contextMenu.MenuItems.Add(statusMenuItem);
-
-      settingsMenuItem.Text = "Settings";
-      settingsMenuItem.Click += new EventHandler(settingsMenuItem_Click);
-      contextMenu.MenuItems.Add(settingsMenuItem);
-
-      contextMenu.MenuItems.Add("-");
-
-      exitMenuItem.Text = "Exit";
-      exitMenuItem.Click += new EventHandler(exitMenuItem_Click);
-      contextMenu.MenuItems.Add(exitMenuItem);
-
-      notifyIcon1.DoubleClick += new EventHandler(statusMenuItem_Click);
-      notifyIcon1.ContextMenu = contextMenu;
-
-      notifierThread = new Thread(notifier);
-      notifierThread.Start();
     }
 
     private Process unisonProcess = new Process();
-    private Thread notifierThread;
-    private ManualResetEvent notifierResetEvent = new ManualResetEvent(false);
-    private bool terminate = false, firstTimeShown = true;
+    private bool firstTimeShown = true;
+    private ManualResetEvent manualResetEvent = new ManualResetEvent(false);
     private delegate void updateStatusDelegate(string line);
     private delegate void clearStatusDelegate();
 
-    private void notifier()
+    private void Form1_Load(object sender, EventArgs e)
     {
-      while (!terminate)
+      backgroundWorker1.RunWorkerAsync();
+      Hide();
+    }
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+      richTextBox1.Width = Width - 40;
+      richTextBox1.Height = Height - 63;
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      if (backgroundWorker1.IsBusy)
       {
-        unisonProcess.Start();
-
-        while (!unisonProcess.HasExited && !terminate)
-        {
-          string line = unisonProcess.StandardError.ReadLine();
-
-          if (line != null)
-          {
-            if (line.Contains("Looking for changes"))
-            {
-              notifyIcon1.Icon = Properties.Resources.SyncingIcon;
-            }
-            else if (line.Contains("Nothing to do"))
-            {
-              notifyIcon1.Icon = Properties.Resources.SyncedIcon;
-            }
-
-            safeInvoke(new updateStatusDelegate(updateStatus), line + Environment.NewLine);
-          }
-        }
-
-        notifyIcon1.Icon = Properties.Resources.ErrorIcon;
-        notifierResetEvent.WaitOne(10000);
-        safeInvoke(new clearStatusDelegate(clearStatus));
+        Hide();
+        e.Cancel = true;
       }
     }
 
-    private void safeInvoke(Delegate method, params object[] args)
-    {
-      if (!terminate)
-      {
-        Invoke(method, args);
-      }
-    }
-
-    private void updateStatus(string line)
-    {
-      richTextBox1.AppendText(line);
-    }
-
-    private void clearStatus()
-    {
-      richTextBox1.Clear();
-    }
-
-    private void statusMenuItem_Click(object sender, EventArgs e)
+    private void statusToolStripMenuItem_Click(object sender, EventArgs e)
     {
       if (firstTimeShown)
       {
@@ -118,7 +61,7 @@ namespace unison_notifier
       Show();
     }
 
-    private void settingsMenuItem_Click(object sender, EventArgs e)
+    private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
     {
       string systemRoot = Environment.ExpandEnvironmentVariables("%systemroot%");
       string homePath = Environment.ExpandEnvironmentVariables("%homepath%");
@@ -126,39 +69,72 @@ namespace unison_notifier
       Process.Start(systemRoot + "\\System32\\notepad.exe", homePath + "\\.unison\\default.prf");
     }
 
-    private void exitMenuItem_Click(object sender, EventArgs e)
+    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
     {
-      terminate = true;
-      Close();
-    }
-
-    private void Form1_Load(object sender, EventArgs e)
-    {
-      Hide();
-    }
-
-    private void Form1_Resize(object sender, EventArgs e)
-    {
-      richTextBox1.Width = Width - 40;
-      richTextBox1.Height = Height - 63;
-    }
-
-    private void Form1_Closing(object sender, FormClosingEventArgs e)
-    {
-      if (!terminate)
+      backgroundWorker1.CancelAsync();
+      if (!unisonProcess.HasExited)
       {
-        Hide();
-        e.Cancel = true;
+        unisonProcess.Kill();
       }
       else
       {
-        notifierResetEvent.Set();
-        if (!unisonProcess.HasExited)
-        {
-          unisonProcess.Kill();
-        }
-        notifierThread.Join();
+        manualResetEvent.Set();
       }
+    }
+
+    private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+    {
+      statusToolStripMenuItem_Click(sender, e);
+    }
+
+    private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+    {
+      while (true)
+      {
+        unisonProcess.Start();
+
+        while (!unisonProcess.HasExited)
+        {
+          string line = unisonProcess.StandardError.ReadLine();
+
+          if (backgroundWorker1.CancellationPending)
+          {
+            return;
+          }
+          if (line.Contains("Looking for changes"))
+          {
+            notifyIcon1.Icon = Properties.Resources.SyncingIcon;
+          }
+          else if (line.Contains("Nothing to do"))
+          {
+            notifyIcon1.Icon = Properties.Resources.SyncedIcon;
+          }
+          Invoke(new updateStatusDelegate(updateStatus), line + Environment.NewLine);
+        }
+
+        notifyIcon1.Icon = Properties.Resources.ErrorIcon;
+        manualResetEvent.WaitOne(10000);
+        if (backgroundWorker1.CancellationPending)
+        {
+          return;
+        }
+        Invoke(new clearStatusDelegate(clearStatus));
+      }
+    }
+
+    private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+      Close();
+    }
+
+    private void updateStatus(string line)
+    {
+      richTextBox1.AppendText(line);
+    }
+
+    private void clearStatus()
+    {
+      richTextBox1.Clear();
     }
   }
 }
